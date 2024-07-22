77b7890d27ad2b18ef3914c67dbc1e39f5ab99c4
blatmen23 <dindilon2000@gmail.com>
2024-07-22 19:36:19 +0300

    leaders update
    вроде работает

--- components/response-row.php
+++ components/response-row.php
@@ -1,4 +1,5 @@
 <div class="response-row">
-    <div><?= $student ?></div>
+    <div><?= $student ?>
+        <?php if ($is_leader) { ?> <span class="label-info">Староста</span><?php } ?></div>
     <div><?= $group ?></div>
 </div>
