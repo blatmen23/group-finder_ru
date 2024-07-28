036e6a5bcf1d2ef0fce4fa3337f9b1f60492b04a
blatmen23 <dindilon2000@gmail.com>
2024-07-28 12:09:38 +0300

    незначительные изменения

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-require "./src/Router.php";
+require "./src/router.php";
 $url = $_SERVER['REQUEST_URI']; // включает /
 //$url = "/" . key($_GET);
 
-$router = new Router();//  /pages/
+$router = new Router(); //  /pages/
 $router->addRoute("/", "main.php"); // добавили главную страницу
 $router->addRoute("/response", "response.php");
 
@@ -16,7 +16,9 @@ $router->route($url);
 $title = "GroupFinder: Index";
 require_once __DIR__ . "/components/head.php";
 ?>
+
 <body>
 
 </body>
+
 </html>
